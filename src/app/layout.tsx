eb5924dacad106651ba7a0feb00591148e33d43f
Areesha Kainat <133228317+Areeesha-Kainat@users.noreply.github.com>
2025-02-13 10:35:39 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,45 @@
+// import type { Metadata } from "next";
+// import { Inter } from "next/font/google";
+// import "./globals.css";
+// import { twMerge } from "tailwind-merge";
+// import { Header } from "@/sections/Header";
+// import ChatBot from "@/components/ChatBot";
+// import { Footer } from "@/sections/Footer";
+
+// const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata: Metadata = {
+//   title: "AGENTIA WORLD",
+//   description: "A landing page for an AI startup created with Frontend Tribe",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en">
+//       <body 
+//       className={twMerge(inter.className, "bg-black text-white antialised")}>
+//         <Header/>
+//         {children}
+//         <ChatBot/>
+//         <Footer/>
+//         </body>
+//       </html>
+    
+//   );
+// }
+
+
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { twMerge } from "tailwind-merge";
 import { Header } from "@/sections/Header";
-import ChatBot from "@/components/ChatBot";
+// import ChatBot from "@/components/ChatBot";
 import { Footer } from "@/sections/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,11 +56,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+        <head>
+        <script src="//code.tidio.co/ksg8fnudiaaterndzs2yadczrymidsvn.js" async></script>
+      </head>
       <body 
       className={twMerge(inter.className, "bg-black text-white antialised")}>
         <Header/>
         {children}
-        <ChatBot/>
+        {/* <ChatBot/> */}
         <Footer/>
         </body>
       </html>
